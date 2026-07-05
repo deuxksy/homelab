b5b907f690a8feef6813e0f17ae8301f311069c2
Crong <deuxksy@gmail.com>
2026-07-06 00:33:45 +0900

    refactor(opentofu): talos 클러스터 제거 (사용자 의도적 삭제 반영)
    - talos.tf 삭제 (talos_master/worker 리소스)
    - variables.tf: talos_* 변수 5개 제거 (template_vmid 포함)
    - outputs.tf: talos_master_mac, talos_worker_mac 제거
    - terraform state rm × 2 (Proxmox에 이미 없음)
    - tofu plan: cockpit 1 add, heritage 1 change-in-place, 0 destroy

--- proxmox/opentofu/outputs.tf
+++ proxmox/opentofu/outputs.tf
@@ -1,13 +1,3 @@
-output "talos_master_mac" {
-  value       = proxmox_virtual_environment_vm.talos_master.mac_addresses[0]
-  description = "talos-master MAC 주소"
-}
-
-output "talos_worker_mac" {
-  value       = proxmox_virtual_environment_vm.talos_worker.mac_addresses[0]
-  description = "talos-worker MAC 주소"
-}
-
 output "heritage_ipv4" {
   value       = proxmox_virtual_environment_container.heritage.ipv4
   description = "heritage LXC IPv4 주소"
--- proxmox/opentofu/variables.tf
+++ proxmox/opentofu/variables.tf
@@ -9,51 +9,9 @@ variable "proxmox_endpoint" {
   default     = "https://walle.bun-bull.ts.net:8006"
 }
 
-variable "talos_template_vmid" {
-  type        = number
-  default     = 900
-  description = "Talos VM 템플릿 ID (사전 수동 생성)"
-}
-
-variable "talos_master_vmid" {
-  type    = number
-  default = 100
-}
-
-variable "talos_worker_vmid" {
-  type    = number
-  default = 101
-}
-
 variable "heritage_vmid" {
   type    = number
   default = 200
-}
-
-variable "talos_master_resources" {
-  type = object({
-    cores  = number
-    memory = number
-    disk   = number
-  })
-  default = {
-    cores  = 2
-    memory = 4096
-    disk   = 20
-  }
-}
-
-variable "talos_worker_resources" {
-  type = object({
-    cores  = number
-    memory = number
-    disk   = number
-  })
-  default = {
-    cores  = 2
-    memory = 2560
-    disk   = 30
-  }
 }
 
 variable "heritage_resources" {
